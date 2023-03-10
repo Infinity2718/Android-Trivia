b47724a2dd9ed1bedd07da8111466209c3100693
tonya <tonyadamwu@gmail.com>
2023-03-09 21:02:14 -0800

    refractors MainActivity and TitleFragment from Kotlin to Java, sets up nav_graph, adds drawer menu

---
+++ app/src/main/java/com/example/android/navigation/MainActivity.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.android.navigation.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavController mNavController;
+    private com.example.android.navigation.databinding.ActivityMainBinding mBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentManager fm = getSupportFragmentManager();
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        NavHostFragment navHostFragment = (NavHostFragment) fm.findFragmentById(R.id.fragmentContainerView);
+        mNavController = navHostFragment.getNavController();
+        mNavController.navigate(R.id.rateUsFragment);
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mBinding.drawerLayout);
+        NavigationUI.setupWithNavController(mBinding.navView, mNavController);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        return NavigationUI.navigateUp(mNavController, mBinding.drawerLayout);
+    }
+}
---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.android.navigation.databinding.FragmentTitleBinding;
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+public class TitleFragment extends Fragment {
+
+    private NavController mNavController;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentTitleBinding binding = FragmentTitleBinding.inflate(inflater);
+        mNavController = NavHostFragment.findNavController(this);
+        binding.playButton.setOnClickListener(v-> {
+            mNavController.navigate(R.id.action_titleFragment_to_gameFragment);
+        });
+        setHasOptionsMenu(true);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.options_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, mNavController);
+    }
+}
